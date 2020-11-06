95ca8db75bee6dc3112161b98eecdce93d5d2a89
chapxls <charlotte.nilsson@hotmail.se>
2020-11-06 23:16:57 +0100

    Fetching data from API and switching image depending on weather

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,21 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import Main from './Main'
-import Sidebar from './Sidebar'
+import Card from './Card/Card'
+import List from './List/List'
 
 
 class App extends Component {
 
-  state = {
-    weather: []
-  }
-
-  // Function that fetches data from API
-  componentDidMount() {
-    fetch('http://wttr.in/Stockholm?format=j1')
-    .then(response => {
-      console.log(response);
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      //this.setState({ weather: data })
-    })
-    .catch(err => {
-      console.log("Error reading data " + err);
-    });
-  }
-
   render () {
     return (
       <Container>
-      <Sidebar/>
-      <Main/>
-    </Container>
+        <SideBar2>
+          <Card/>
+        </SideBar2>
+        <Main2>
+          <List/>
+        </Main2>
+      </Container>
     );
   }
 }
@@ -39,6 +23,17 @@ class App extends Component {
 const Container = styled.div `
   display: flex; 
   margin: 10px;
+`
+
+const SideBar2 = styled.div `
+  width: 30%;
+  height: 100%;
+  background: #f1f8ff;
+`
+
+const Main2 = styled.div `
+height: 100%;
+  width: 544px;
 `
 
 export default App
